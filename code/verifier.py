14f925a0a283e0040750256c3fadc746b478a619
Oscar Pitcho <33069455+oscarpitcho@users.noreply.github.com>
2025-09-19 17:23:21 -0600

    Update verifier.py

--- code/verifier.py
+++ code/verifier.py
@@ -13,12 +13,9 @@ import numpy as np
 logger = logging.getLogger(__name__)
 
 DEVICE = "cpu"
-
 def verify_bounds(lower_bounds, true_label):
-
     return (lower_bounds[:true_label] > 0).all() and (lower_bounds[true_label + 1:] > 0).all()
-
-
+    
 def analyze(
     net: torch.nn.Module, inputs: torch.Tensor, eps: float, true_label: int
 ) -> bool:
@@ -30,17 +27,16 @@ def analyze(
         inputs - eps, min=0.0).to(DEVICE)
     upper_bounds_init = torch.clamp(
         inputs + eps, max=1.0).to(DEVICE)
-
-
+    
     #We find the number of classes by looking at the output shape of the network.
     nb_classes = net(inputs.unsqueeze(0)).shape[1]
 
+    #Building additional verification layer, new neurons are all defined as o_true - o_i
+    #We keep bias for interopperability with other layers
     verification_layer = torch.nn.Linear(nb_classes, nb_classes, bias=True)
-
-
     true_label_matrix = torch.zeros(nb_classes, nb_classes)
     true_label_matrix[:, true_label] = 1
-    layer_weights = true_label_matrix - torch.eye(nb_classes)
+    layer_weights = true_label_matrix - torch.eye(nb_classes) #o_true - o_i
 
     verification_layer.weight.data = layer_weights.to(DEVICE)
     verification_layer.bias.data = torch.zeros(nb_classes).to(DEVICE)
@@ -50,23 +46,16 @@ def analyze(
     logger.info(f"Network analyzed - {net}")
     dp = DeepPoly(net, input_size)
 
-
-
-
     train_beta = any([isinstance(layer, ReLuTransformer) or isinstance(layer, LeakyReLuTransformer) for layer in dp.verifier_net])
 
     #################################
     '''TRAINING OF BETA PARAMETER '''
     lr = 0.8
     epochs = 20
-       
-
     if train_beta:
         logger.info("---------Training beta parameter------------")
-
         torch.autograd.set_detect_anomaly(True)
-
-
+        
         #We freeze the weights of the network and only optimize the beta parameter of the relu transformer.
         for param in dp.verifier_net.parameters():
             param.requires_grad = False
@@ -74,59 +63,42 @@ def analyze(
         for layer in dp.verifier_net:
             if isinstance(layer, ReLuTransformer) or isinstance(layer, LeakyReLuTransformer):
                 layer.beta.requires_grad = True
-            
-
+                
         optimizer = torch.optim.Adam(
                     dp.verifier_net.parameters(), 
                     lr=lr # Learning rate
                 )
         
         scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=3, verbose=True)
-
-
+        
         for epoch in range(epochs):
             logger.info(f"#### Epoch {epoch}")
 
             ver_lb, _ = dp(lower_bounds_init, upper_bounds_init)
-            
-            #Look if the network is verified after forward pass
             is_verified = verify_bounds(ver_lb, true_label)
-
-
+            
             if is_verified:
                 print(f"Stopped after {epoch} epochs - after backsub")
                 return is_verified
-            
-            
 
+            # Select all activations except true label
             invalid_classes_activations = torch.cat((ver_lb[:true_label], ver_lb[true_label + 1:]), dim=0)
 
             logger.info(f"Invalid classes activations: {invalid_classes_activations}")
-
-    
-
-            loss = -torch.sum(invalid_classes_activations)
-
+            loss = - torch.sum(invalid_classes_activations)
 
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
             scheduler.step(loss)
-
-
             logger.info(f"Epoch {epoch} - loss: {loss.item()}")
          
-         
         logger.info("---------Training beta parameter finished------------")
-
-
     ver_lb, _ = dp(lower_bounds_init, upper_bounds_init)
-
     is_verified = verify_bounds(ver_lb, true_label)
 
     return is_verified
-
-
+    
 def main():
     start = time()
 
@@ -156,7 +128,6 @@ def main():
     )
     parser.add_argument("--spec", type=str, required=True, help="Test case to verify.")
     parser.add_argument('-l', '--log', dest='loglevel', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], help="Set the logging level (default: %(default)s)", default='WARNING')
-
     args = parser.parse_args()
 
     true_label, dataset, image, eps = parse_spec(args.spec)
